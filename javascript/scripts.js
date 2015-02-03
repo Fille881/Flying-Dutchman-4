4130cd97d67aae4398cc74ecc4b339108fe60092
FunnyFrank <tomfledderus@gmail.com>
2015-02-03 13:16:29 +0100

    added script

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,12 @@
-test
+<SCRIPT LANGUAGE="JavaScript">
+
+var userName = prompt("What is your name?")
+
+var popup = function(userName){
+	
+	console.log("Good afternoon" + userName);
+	
+	}
+	
+	
+</SCRIPT>
