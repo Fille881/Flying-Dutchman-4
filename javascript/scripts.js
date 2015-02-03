53ba3cf36412002a6ad8de05105e48d1c01a3538
FunnyFrank <tomfledderus@gmail.com>
2015-02-03 14:06:14 +0100

    changes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,8 @@
-<script type="text/javascript">
-       
+
 	   popup = function(userName){
 		   
 		   var userName = prompt("What is your name?");
 		   alert("Good afternoon" + " " + userName);
 	
 	}
-        </script>
+    
