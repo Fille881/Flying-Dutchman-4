8c1057868a2e1d884dd7d770de24467f4c9b1f8a
FunnyFrank <tomfledderus@gmail.com>
2015-02-03 14:19:16 +0100

    changes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,8 +1,16 @@
 
+/*
 	   popup = function(userName){
 		   
 		   var userName = prompt("What is your name?");
 		   alert("Good afternoon" + " " + userName);
 	
 	}
+*/
     
+    makeOrder = function(){
+	    var numberOfBeers = 1;
+	    return numberOfBeers;
+    }
+    
+    document.getElementById("showOrders").innerHTML=makeOrder()
